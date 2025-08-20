40aef4649d57290ea30e7712e1aae2f4dd42939f
Andrei Kuznetsov <awakum192@gmail.com>
2025-08-20 17:30:53 +0300

    feat: numba implemented

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
 import time
-import numpy
+import numpy as np
 import argparse
 from neural_network import NeuralNetwork
 from utils import load_file, prepare_mnist_line, plot_ascii
 
 
 if __name__ == '__main__':
-    # Parse command line arguments
     parser = argparse.ArgumentParser(description='Train and test a neural network on MNIST data.')
-    parser.add_argument('hidden_nodes', type=int, nargs='?', default=200, help='Number of nodes in the hidden layer (default: 200)')
-    parser.add_argument('learning_rate', type=float, nargs='?', default=0.3, help='Learning rate for the neural network (default: 0.3)')
+    parser.add_argument('--hidden-nodes', type=int, default=200, help='Number of nodes in the hidden layer (default: 200)')
+    parser.add_argument('--learning-rate', type=float, default=1.7, help='Learning rate for the neural network (default: 0.3)')
     parser.add_argument('--epochs', type=int, default=1, help='Number of training epochs (default: 1)')
+    parser.add_argument('--batch-size', type=int, default=60, help='Batch size for training (default: 100)')
     args = parser.parse_args()
 
     start_time = time.time()
-    input_nodes = 784  # 28 weight x 28 height pixels
-    output_nodes = 10  # we need a result in range of 0 - 9
+    input_nodes = 784
+    output_nodes = 10
     
     print(f"Initializing network with: hidden_nodes={args.hidden_nodes}, learning_rate={args.learning_rate}")
     network = NeuralNetwork(input_nodes, args.hidden_nodes, output_nodes, learning_rate=args.learning_rate)
     
-    # training
+    # Load and prepare training data
     content = load_file('mnist_train.csv', as_lines=True)
+    training_data = [prepare_mnist_line(line) for line in content]
+    
     for epoch in range(args.epochs):
         print(f"Starting epoch {epoch + 1}/{args.epochs}")
-        for line in content:
-            inputs, targets = prepare_mnist_line(line)
-            network.train(inputs, targets)
+        
+        # Process in batches
+        for i in range(0, len(training_data), args.batch_size):
+            batch = training_data[i:i + args.batch_size]
+            inputs_batch = np.array([data[0] for data in batch])
+            targets_batch = np.array([data[1] for data in batch])
+            
+            network.train_batch(inputs_batch, targets_batch)
 
     # testing
     content = load_file('mnist_test.csv', as_lines=True)
     score = 0
     for line in content:
         inputs, targets = prepare_mnist_line(line)
-        response = numpy.argmax(network.query(inputs))
+        response = np.argmax(network.query(inputs))
         answer = int(line[0])
         if response == answer:
             score += 1
--- neural_network.py
+++ neural_network.py
@@ -1,9 +1,10 @@
 from typing import Callable
-import numpy
+import numpy as np
 from numpy._typing import NDArray
 from numpy import float64
 import scipy
 from dataclasses import dataclass
+from numba import njit
 
 
 @dataclass
@@ -16,53 +17,56 @@ class NeuralNetwork:
     activation_function: Callable = lambda x: scipy.special.expit(x)
 
     def __post_init__(self) -> None:
-        # строим матрицы весовых коэффициентов
-        # количество строк в каждой матрице определяется первым множителем, соотв. колтичеству узлов в слое, куда поступают сигналы
-        # поэтому input -> hidden; hidden -> output
-        # выбираем Гауссово распределение (random.normal), чтобы минимизировать количество весов с крайними значениями диапазона
-        # возводим в степень -0.5 (pow)
-        self.from_input_to_hidden_matrix_weights = numpy.random.normal(0.0, pow(self.hidden_nodes, -0.5), (self.hidden_nodes, self.input_nodes))
-        self.from_hidden_to_output_matrix_weights = numpy.random.normal(0.0, pow(self.output_nodes, -0.5), (self.output_nodes, self.hidden_nodes))
+        self.wih = np.random.normal(0.0, pow(self.hidden_nodes, -0.5), (self.hidden_nodes, self.input_nodes))
+        self.who = np.random.normal(0.0, pow(self.output_nodes, -0.5), (self.output_nodes, self.hidden_nodes))
 
-    def train(self, inputs_list: NDArray[float64], targets_list: NDArray[float64]) -> None:
-        # преобразовать список входных значений в двухмерный массив
-        inputs = numpy.array(inputs_list, ndmin=2).T
-        targets = numpy.array(targets_list, ndmin=2).T
+    def train_batch(self, inputs_batch: NDArray[float64], targets_batch: NDArray[float64]) -> None:
+        """Train on a batch of samples for better performance"""
+        self.wih, self.who = train_batch(inputs_batch, targets_batch, self.wih, self.who, self.learning_rate)
 
-        # рассчитать входящие сигналы для скрытого слоя
-        hidden_inputs = numpy.dot(self.from_input_to_hidden_matrix_weights, inputs)
-        # рассчитать исходящие сигналы для скрытого слоя
-        hidden_outputs = self.activation_function(hidden_inputs)
-
-        # рассчитать входящие сигналы для выхоного слоя
-        final_inputs = numpy.dot(self.from_hidden_to_output_matrix_weights, hidden_outputs)
-        # рассчитать исходящие сигналы для выходного слоя
-        final_outputs = self.activation_function(final_inputs)
-
-        # ошибки выходного слоя = (целевое значение - фактическое значение)
-        output_errors = targets - final_outputs
-        # ошибки скрытого слоя - это ошибки output_errors, распределенные пропорционально весовым
-        # коэффициентам связей и рекомбинированные на скрытых узлах
-        hidden_errors = numpy.dot(self.from_hidden_to_output_matrix_weights.T, output_errors)
-
-        # обновляем весовые коэффициенты для связей между скрытым и выходным слоями
-        self.from_hidden_to_output_matrix_weights += self.learning_rate * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
-
-        # обновляем весовые коэффициенты для связей между входным и скрытым слоями
-        self.from_input_to_hidden_matrix_weights += self.learning_rate * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
-
-    # опрос нейронной сети
     def query(self, inputs: NDArray[float64]) -> NDArray[float64]:
-        # преобразовать список входных значений в двумерный массив
-        inputs = numpy.array(inputs, ndmin=2).T
+        return query_single(inputs, self.wih, self.who)
 
-        # рассчитать входящие сигналы для скрытого слоя
-        hidden_inputs = numpy.dot(self.from_input_to_hidden_matrix_weights, inputs)
-        # рассчитать исходящие сигналы для скрытого слоя
-        hidden_outputs = self.activation_function(hidden_inputs)
 
-        # рассчитать входящие сигналы для выходного слоя
-        final_inputs = numpy.dot(self.from_hidden_to_output_matrix_weights, hidden_outputs)
-        # рассчитать исхоящие сигналы для выходного слоя
-        final_outputs = self.activation_function(final_inputs)
-        return final_outputs
+@njit
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+@njit
+def train_batch(inputs_batch: NDArray[float64], targets_batch: NDArray[float64], 
+                wih: NDArray[float64], who: NDArray[float64], learning_rate: float):
+    """
+    Train on a batch of samples - much more efficient
+    inputs_batch: shape (batch_size, input_nodes)
+    targets_batch: shape (batch_size, output_nodes)
+    """
+    batch_size = inputs_batch.shape[0]
+    
+    # Forward pass for entire batch
+    hidden_inputs = np.dot(inputs_batch, wih.T)  # (batch_size, hidden_nodes)
+    hidden_outputs = sigmoid(hidden_inputs)
+    final_inputs = np.dot(hidden_outputs, who.T)  # (batch_size, output_nodes)
+    final_outputs = sigmoid(final_inputs)
+
+    # Backward pass
+    output_errors = targets_batch - final_outputs  # (batch_size, output_nodes)
+    hidden_errors = np.dot(output_errors, who)  # (batch_size, hidden_nodes)
+
+    # Calculate weight updates (matrix operations for entire batch)
+    who_update = learning_rate * np.dot((output_errors * final_outputs * (1.0 - final_outputs)).T, hidden_outputs) / batch_size
+    wih_update = learning_rate * np.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)).T, inputs_batch) / batch_size
+
+    # Update weights
+    who += who_update
+    wih += wih_update
+    
+    return wih, who
+
+@njit
+def query_single(inputs, wih, who):
+    inputs = inputs.reshape(1, -1)  # row vector for batch compatibility
+    hidden_inputs = np.dot(inputs, wih.T)
+    hidden_outputs = sigmoid(hidden_inputs)
+    final_inputs = np.dot(hidden_outputs, who.T)
+    final_outputs = sigmoid(final_inputs)
+    return final_outputs[0]  # return first (and only) result
